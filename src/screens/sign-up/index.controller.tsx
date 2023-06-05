252b463e4d2454a7d346ded09cc352e82720cb87
Yaslim2 <yaslimsoares15@gmail.com>
2023-06-05 13:43:30 -0300

    refactor: move logic of steps to controller

---
+++ src/screens/sign-up/index.controller.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+import Text from '@/shared/components/Text';
+import { ThemeOverride } from '@/shared/styles/theme';
+import { MainStackTypes, SignUpStackTypes } from '@/shared/types/navigation';
+
+import { useTheme } from 'styled-components';
+
+const useController = ({
+  navigation,
+  route,
+}: MainStackTypes.RouteProps<MainStackTypes.Routes.SignUp>) => {
+  const theme = useTheme() as ThemeOverride;
+  const stepsStyle = {
+    stepIndicatorSize: 35,
+    currentStepIndicatorSize: 40,
+    separatorStrokeWidth: 2,
+    currentStepStrokeWidth: 2.5,
+    stepStrokeCurrentColor: theme.colors.orange.primary,
+    stepStrokeWidth: 2,
+    separatorStrokeFinishedWidth: 4,
+    stepStrokeFinishedColor: theme.colors.orange.primary,
+    stepStrokeUnFinishedColor: theme.colors.darkGrey.primary,
+    separatorFinishedColor: theme.colors.orange.primary,
+    separatorUnFinishedColor: theme.colors.darkGrey.primary,
+    stepIndicatorFinishedColor: theme.colors.orange.primary,
+    stepIndicatorUnFinishedColor: theme.colors.background,
+    stepIndicatorCurrentColor: theme.colors.background,
+    stepIndicatorLabelCurrentColor: theme.colors.orange.primary,
+  };
+
+  const renderStepIndicator = ({
+    position,
+    stepStatus,
+  }: {
+    position: number;
+    stepStatus: string;
+  }) => {
+    const color =
+      stepStatus === 'finished'
+        ? theme.colors.white.primary
+        : theme.colors.orange.primary;
+
+    switch (position) {
+      case 0: {
+        return (
+          <MaterialCommunityIcons color={color} size={20} name="account" />
+        );
+      }
+      case 1: {
+        return (
+          <MaterialCommunityIcons
+            color={color}
+            size={18}
+            name="cellphone-lock"
+          />
+        );
+      }
+      case 2: {
+        return <MaterialCommunityIcons color={color} size={18} name="camera" />;
+      }
+      case 3:
+        return <MaterialCommunityIcons color={color} size={15} name="check" />;
+      default: {
+        break;
+      }
+    }
+  };
+
+  const renderLabel = ({
+    label,
+    stepStatus,
+  }: {
+    position: number;
+    stepStatus: string;
+    label: string;
+    currentPosition: number;
+  }) => {
+    const color =
+      stepStatus === 'finished'
+        ? theme.colors.darkGrey.primary
+        : stepStatus !== 'current'
+        ? theme.colors.darkGrey.primary
+        : theme.colors.orange.primary;
+
+    return (
+      <Text font="secondary" weight="bold" color={color} size={13}>
+        {label}
+      </Text>
+    );
+  };
+  const routeName = getFocusedRouteNameFromRoute(route);
+
+  const handleClose = (): void =>
+    navigation.reset({ routes: [{ name: MainStackTypes.Routes.Onboarding }] });
+
+  const goBack = (): void => {
+    switch (routeName) {
+      case SignUpStackTypes.Routes.SetUserInfo:
+        handleClose();
+        break;
+      case SignUpStackTypes.Routes.SetProfileImage:
+        navigation.navigate(SignUpStackTypes.Routes.SetUserInfo);
+        break;
+      case SignUpStackTypes.Routes.SetPhoneNumber:
+        navigation.navigate(SignUpStackTypes.Routes.SetProfileImage);
+        break;
+      default:
+        handleClose();
+        break;
+    }
+  };
+
+  return {
+    goBack,
+    routeName,
+    stepsStyle,
+    renderLabel,
+    renderStepIndicator,
+  };
+};
+
+export default useController;
--- src/screens/sign-up/index.tsx
+++ src/screens/sign-up/index.tsx
@@ -1,98 +1,25 @@
 import * as React from 'react';
 import StepIndicator from 'react-native-step-indicator';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import Text from '@/shared/components/Text';
-import colors from '@/shared/styles/colors';
 import { MainStackTypes } from '@/shared/types/navigation';
 
 import SignUpStack from './components/stack';
 import useController from './index.controller';
 import { Container } from './styles';
 
-const secondIndicatorStyles = {
-  stepIndicatorSize: 35,
-  currentStepIndicatorSize: 40,
-  separatorStrokeWidth: 2,
-  currentStepStrokeWidth: 3,
-  stepStrokeCurrentColor: colors.orange.primary,
-  stepStrokeWidth: 2,
-  separatorStrokeFinishedWidth: 4,
-  stepStrokeFinishedColor: colors.orange.primary,
-  stepStrokeUnFinishedColor: colors.darkGrey.primary,
-  separatorFinishedColor: colors.orange.primary,
-  separatorUnFinishedColor: colors.darkGrey.primary,
-  stepIndicatorFinishedColor: colors.orange.primary,
-  stepIndicatorUnFinishedColor: colors.grey.primary,
-  stepIndicatorCurrentColor: colors.grey.primary,
-  stepIndicatorLabelCurrentColor: colors.orange.primary,
-};
-
-const renderStepIndicator = ({
-  position,
-  stepStatus,
-}: {
-  position: number;
-  stepStatus: string;
-}) => {
-  const color =
-    stepStatus === 'finished' ? colors.white.primary : colors.orange.primary;
-
-  switch (position) {
-    case 0: {
-      return <MaterialCommunityIcons color={color} size={20} name="account" />;
-    }
-    case 1: {
-      return (
-        <MaterialCommunityIcons color={color} size={18} name="cellphone-lock" />
-      );
-    }
-    case 2: {
-      return <MaterialCommunityIcons color={color} size={18} name="camera" />;
-    }
-    case 3:
-      return <MaterialCommunityIcons color={color} size={15} name="check" />;
-    default: {
-      break;
-    }
-  }
-};
-
-const renderLabel = ({
-  label,
-  stepStatus,
-}: {
-  position: number;
-  stepStatus: string;
-  label: string;
-  currentPosition: number;
-}) => {
-  const color =
-    stepStatus === 'finished'
-      ? colors.darkGrey.primary
-      : stepStatus !== 'current'
-      ? colors.darkGrey.primary
-      : colors.orange.primary;
-
-  return (
-    <Text font="secondary" weight="bold" color={color} size={13}>
-      {label}
-    </Text>
-  );
-};
-
 const SignUpScreen: MainStackTypes.ComponentProps<
   MainStackTypes.Routes.SignUp
 > = ({ navigation, route }) => {
-  const { routeName } = useController({
-    navigation,
-    route,
-  });
+  const { routeName, stepsStyle, renderLabel, renderStepIndicator } =
+    useController({
+      navigation,
+      route,
+    });
 
   return (
     <Container routeName={routeName}>
       <StepIndicator
-        customStyles={secondIndicatorStyles}
+        customStyles={stepsStyle}
         renderStepIndicator={renderStepIndicator}
         stepCount={4}
         currentPosition={4}
